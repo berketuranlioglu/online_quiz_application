8ac3d5e1338a6619388a983f4b8ed6f29a8e1edf
Berke Turanlıoğlu <berke.turanlioglu@gmail.com>
2022-11-28 16:24:25 +0300

    all code

---
+++ quizserver/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace quizserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_port = new System.Windows.Forms.Label();
+            this.textbox_port = new System.Windows.Forms.TextBox();
+            this.control_panel = new System.Windows.Forms.RichTextBox();
+            this.button_listen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbox_noquestion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(51, 38);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(32, 15);
+            this.label_port.TabIndex = 0;
+            this.label_port.Text = "Port:";
+            // 
+            // textbox_port
+            // 
+            this.textbox_port.Location = new System.Drawing.Point(89, 35);
+            this.textbox_port.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textbox_port.Name = "textbox_port";
+            this.textbox_port.Size = new System.Drawing.Size(110, 23);
+            this.textbox_port.TabIndex = 1;
+            this.textbox_port.Text = "11";
+            // 
+            // control_panel
+            // 
+            this.control_panel.Location = new System.Drawing.Point(41, 139);
+            this.control_panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.control_panel.Name = "control_panel";
+            this.control_panel.Size = new System.Drawing.Size(418, 156);
+            this.control_panel.TabIndex = 2;
+            this.control_panel.Text = "";
+            // 
+            // button_listen
+            // 
+            this.button_listen.Location = new System.Drawing.Point(191, 80);
+            this.button_listen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_listen.Name = "button_listen";
+            this.button_listen.Size = new System.Drawing.Size(82, 22);
+            this.button_listen.TabIndex = 3;
+            this.button_listen.Text = "Connect";
+            this.button_listen.UseVisualStyleBackColor = true;
+            this.button_listen.Click += new System.EventHandler(this.button_listen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of Questions:";
+            // 
+            // textbox_noquestion
+            // 
+            this.textbox_noquestion.Location = new System.Drawing.Point(386, 35);
+            this.textbox_noquestion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textbox_noquestion.Name = "textbox_noquestion";
+            this.textbox_noquestion.Size = new System.Drawing.Size(41, 23);
+            this.textbox_noquestion.TabIndex = 5;
+            this.textbox_noquestion.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 338);
+            this.Controls.Add(this.textbox_noquestion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_listen);
+            this.Controls.Add(this.control_panel);
+            this.Controls.Add(this.textbox_port);
+            this.Controls.Add(this.label_port);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label_port;
+        private TextBox textbox_port;
+        private RichTextBox control_panel;
+        private Button button_listen;
+        private Label label1;
+        private TextBox textbox_noquestion;
+    }
+}
---
+++ quizserver/Form1.cs
@@ -0,0 +1,342 @@
+using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace quizserver
+{
+    public partial class Form1 : Form
+    {
+
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //List<Socket> clientSockets = new List<Socket>();
+
+        List<String> questions = new List<String>();
+        List<int> answers = new List<int>();
+
+        bool terminating = false;
+        bool listening = false;
+        bool canbeplayer = true;
+        int noquestion;
+        bool gameFinished = false;
+
+
+        struct client
+        {
+            public Socket client_socket;
+            public String client_name;
+        }
+
+
+        struct player
+        {
+            public String name;
+            public List<double> answers;
+            public double score;
+
+        }
+
+        struct serverAnswerItem
+        {
+            public player player1;
+            public player player2;
+
+        }
+
+        List<client> clientList = new List<client>(); //clients with socket and name
+        List<player> playerList = new List<player>(); //users with name, answer and score
+
+        List<serverAnswerItem> serverAnswerList = new List<serverAnswerItem>(); //the list to keep the answers of users
+
+        // List<serverAnswerItem> serverAnswerList = new List<serverAnswerItem>(); 
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+
+
+        private void button_listen_Click(object sender, EventArgs e)
+        {
+            //TO DO: TRY CATCH EKLENSIN BURAYA DIREKT IF YERINE
+
+            noquestion = Int32.Parse(textbox_noquestion.Text); //number of questions
+
+
+            //*****QUESTION AND ANSWER READING FROM FILE
+
+            var readlines = File.ReadLines("questions.txt");
+            int counter = 0;
+            foreach (var line in readlines)
+            {
+                if (counter % 2 == 0) //questions from file
+                {
+                    questions.Add(line);
+
+                }
+                else //answers from file
+                {
+                    int answertemp = Int32.Parse(line);
+                    answers.Add(answertemp);
+                }
+                counter++;
+            }
+
+            //*****
+
+
+            int serverPort;
+
+            if (Int32.TryParse(textbox_port.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(2); //listen two clients for now
+
+                listening = true;
+                button_listen.Enabled = false;
+
+
+                Thread acceptThread = new Thread(Accept);
+                acceptThread.Start();
+
+                control_panel.AppendText("Started listening on port: " + serverPort + "\n");
+
+            }
+            else
+            {
+                control_panel.AppendText("Please check port number \n");
+            }
+
+        }
+
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    //create client with its socket
+                    client newClient;
+                    newClient.client_socket = serverSocket.Accept();
+
+                    //take its name
+                    Byte[] nameBuffer = new Byte[64];
+                    newClient.client_socket.Receive(nameBuffer);
+
+                    //the name sent by client
+                    string incomingName = Encoding.Default.GetString(nameBuffer);
+                    incomingName = incomingName.Substring(0, incomingName.IndexOf('\0'));
+
+
+                    //check here if the name is already in clients
+                    foreach (client comer in clientList)
+                    {
+                        if (comer.client_name.Equals(incomingName))
+                        {
+                            canbeplayer = false;
+                            break;
+                        }
+                    }
+
+                    //the client added to list, port and name is known
+                    newClient.client_name = incomingName;
+                    clientList.Add(newClient);
+
+                    //if not a existing client, create new player
+                    if (canbeplayer == true)
+                    {
+                        control_panel.AppendText("Player " + newClient.client_name + " connected.\n");
+                        player newPlayer;
+                        newPlayer.name = newClient.client_name;
+                        newPlayer.score = 0;
+                        newPlayer.answers = new List<double>();
+
+                        playerList.Add(newPlayer);
+
+                        //successfully start thread with client
+                        Thread receiveThread = new Thread(() => Receive(newClient));
+                        receiveThread.Start();
+
+
+
+                    }
+                    else
+                    {
+                        control_panel.AppendText("Player " + newClient.client_name + " already exists.\n");
+                        newClient.client_socket.Close();
+                        clientList.Remove(newClient);
+                    }
+
+
+
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        control_panel.AppendText("The socket stopped working.\n");
+                    }
+
+                }
+            }
+        }
+
+
+        private void Receive(client newClient)
+        {
+
+            //client run this receive function right now, we have name and socket
+
+            client newComer = clientList[clientList.Count() - 1];
+
+            bool connected = true;
+            //send message to inform game starts
+            if (playerList.Count == 2)
+            {
+                Byte[] gameStart = new Byte[64];
+                gameStart = Encoding.Default.GetBytes("SERVER: Welcome to the game. We are starting...\n");
+                control_panel.AppendText("SERVER: Welcome to the game. We are starting...\n");
+                
+            }
+
+            while (connected)
+            {
+                try
+                {
+
+                    //if 2 player is joined
+                    //ask questions
+                    //check if everyone answered
+                    //check answers
+                    //announce winner
+                    //list the score table
+
+
+                    if (playerList.Count == 2)
+                    {
+                        Byte[] gameStart = new Byte[64];
+                        gameStart = Encoding.Default.GetBytes("SERVER: Welcome to the game. We are starting...\n");
+                        newClient.client_socket.Send(gameStart);
+
+                        //while # of questions are finished
+                        for (int i = 0; i < noquestion; i++)
+                        {
+                            int answeredNum = 0;
+                            Byte[] qBuffer = new Byte[64];
+                            qBuffer = Encoding.Default.GetBytes(questions[i % (questions.Count())] + "\n");
+                            newClient.client_socket.Send(qBuffer);
+                            if (control_panel.Find(questions[i]) != null)
+                            {
+                                control_panel.AppendText("Server: " + questions[i % (questions.Count())] + "\n");
+                            }
+
+                            Byte[] aBuffer = new Byte[64];
+                            newClient.client_socket.Receive(aBuffer);
+                            String incomingAnswer = Encoding.Default.GetString(aBuffer);
+                            incomingAnswer = incomingAnswer.Substring(0, incomingAnswer.IndexOf('\0'));
+                            double playerAnswer = Convert.ToDouble(incomingAnswer);
+
+                            foreach (player plyr in playerList)
+                            {
+                                if (newClient.client_name == plyr.name)
+                                {
+                                    plyr.answers.Add(playerAnswer);
+                                    answeredNum++;
+                                }
+                            }
+
+
+                            if (playerAnswer == answers[i % (answers.Count())])
+                            {
+                                control_panel.AppendText("Server: we have winner \n");
+                            }
+
+                        }
+
+                    }
+
+
+
+
+
+                    /*
+                    //Send question to client
+                    Byte[] questionBuffer = new Byte[64];
+                    questionBuffer = Encoding.Default.GetBytes("Question 1. What is answer?");
+                    newClient.client_socket.Send(questionBuffer);
+                    control_panel.AppendText("Question 1. What is answer?\n");
+
+                    Byte[] comingAnswer = new Byte[64];
+                    newClient.client_socket.Receive(comingAnswer);
+                    string incomingMessage = Encoding.Default.GetString(comingAnswer);
+                    string answer = incomingMessage.Substring(incomingMessage.IndexOf(' ') + 1, incomingMessage.IndexOf('\0'));
+                    answer = answer.Substring(0, answer.IndexOf('\0'));
+                    control_panel.AppendText("Client named " + newClient.client_name + ", answer is: " + answer + "\n");
+
+
+
+                    //thisClient.Receive(buffer); 
+                    //string incomingMessage = Encoding.Default.GetString(buffer);
+                    //incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+                    //control_panel.AppendText("Client named: " + incomingMessage + " is connected.\n");
+                    */
+
+
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        control_panel.AppendText("A client has disconnected\n");
+                        //inform the other player about disconnection
+                        //make it winner
+                    }
+                    newClient.client_socket.Close();
+                    clientList.Remove(newClient);
+                    connected = false;
+                }
+            }
+        }
+
+        private void checkWinner()
+        {
+
+        }
+
+        private void listDescending()
+        {
+
+        }
+
+
+
+        //if exit button is clicked
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+    }
+}
